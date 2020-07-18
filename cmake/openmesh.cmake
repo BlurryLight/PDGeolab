e36596840248d120f3f0c3e45070a20e41d188bd
panda <zhonghaoyu555@gmail.com>
2020-07-18 13:27:59 +0800

    build openmesh in Release mode

--- cmake/openmesh.cmake
+++ cmake/openmesh.cmake
@@ -3,5 +3,9 @@ FetchContent_Declare(
         GIT_REPOSITORY https://www.graphics.rwth-aachen.de:9000/OpenMesh/OpenMesh.git
         GIT_TAG OpenMesh-8.1
 )
-
-FetchContent_MakeAvailable(openmesh)
+if(NOT openmesh_POPULATED)
+    FetchContent_Populate(openmesh)
+    set(CMAKE_BUILD_TYPE Release)
+    add_subdirectory(${openmesh_SOURCE_DIR} ${openmesh_BINARY_DIR})
+    set(CMAKE_BUILD_TYPE)
+endif()
